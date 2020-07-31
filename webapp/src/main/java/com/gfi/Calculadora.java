f90190e9ca03e39c1f1249ba0e7ba8e78b2e760e
Oray <oscar.ray.franco@gmail.com>
2020-07-31 10:57:41 +0000

    plugin jacoco configuration

--- webapp/src/main/java/com/gfi/Calculadora.java
+++ webapp/src/main/java/com/gfi/Calculadora.java
@@ -1,7 +1,12 @@
 package com.gfi;
 
 public class Calculadora{
-
+    
+    /**
+     * Esta función suma dos números
+     * 
+     * 
+     */
     public static int suma(int numero1, int numero2){
         return numero1+numero2; // No escribo el codigo
     }
